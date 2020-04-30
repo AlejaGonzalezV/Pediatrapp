dec02dde8818a6a8c5a31cc7ac277ec2c79e36ad
AlejaGonzalezV <aleja.gonzalezv19@gmail.com>
2020-04-30 14:31:59 -0500

    Login design changes, SignUp activity created

--- app/src/main/java/com/example/pediatrapp/view/LoginActivity.java
+++ app/src/main/java/com/example/pediatrapp/view/LoginActivity.java
@@ -3,18 +3,47 @@ package com.example.pediatrapp.view;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.pediatrapp.R;
 
 public class LoginActivity extends AppCompatActivity {
 
-    
+    private TextView email, password;
+    private Button signIn, signUp;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.password);
+        signIn = findViewById(R.id.signIn);
+        signUp = findViewById(R.id.signUp);
+        signIn.setOnClickListener(
+
+                (v) -> {
+
+
+
+                }
+
+        );
+
+        signUp.setOnClickListener(
+
+                (v) -> {
+
+                
+
+
+                }
+
+        );
+
+
     }
 
 
---
+++ app/src/main/java/com/example/pediatrapp/view/SignUpActivity.java
@@ -0,0 +1,15 @@
+package com.example.pediatrapp.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.example.pediatrapp.R;
+
+
+public class SignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+}
